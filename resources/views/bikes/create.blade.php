76d162eedfcfef55a88eabf92ad95ae37b6517b2
Paco Jaez <58423341+pacojaez@users.noreply.github.com>
2022-10-06 15:15:50 +0200

    added funcionality to create bike

--- resources/views/bikes/create.blade.php
+++ resources/views/bikes/create.blade.php
@@ -95,6 +95,12 @@
                         id="image" type="file" accept="image/jpg, image/png, image/jpeg"
                         name="image" > --}}
                 </div>
+                <div class="flex flex-col justify-center px-3 md:w-1/2">
+                    <h2>PREVIEW</h2>
+                    <div class="px-3 preview md:w-1/2">
+                        <p>No hay preseleccionada ninguna imagen</p>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="flex flex-row justify-center">
@@ -107,6 +113,84 @@
         matricula.disabled = !matriculada.checked;
         matriculada.onchange = function(){
             matricula.disabled = !matriculada.checked;
+
+        }
+        const input = document.getElementById('inputFile');
+        const preview = document.querySelector('.preview');
+        input.style.opacity = 80;
+
+        input.addEventListener('change', updateImageDisplay);
+
+        function updateImageDisplay() {
+            while (preview.firstChild) {
+                preview.removeChild(preview.firstChild);
+            }
+
+            const curFiles = input.files;
+            if (curFiles.length === 0) {
+                const para = document.createElement('p');
+                para.textContent = 'No hay imagenes preseleccionadas';
+                preview.appendChild(para);
+            } else {
+                const list = document.createElement('ol');
+                preview.appendChild(list);
+
+                for (const file of curFiles) {
+                    const listItem = document.createElement('li');
+                    const para = document.createElement('p');
+                    if (validFileType(file)) {
+                        para.textContent = `File name ${file.name}, Tamaño ${returnFileSize(file.size)}.`;
+                        const image = document.createElement('img');
+                        image.src = URL.createObjectURL(file);
+
+                        listItem.appendChild(image);
+                        listItem.appendChild(para);
+                    } else {
+                        para.textContent = `File name ${file.name}: No es un tipo de imagen válido. Selecciona una imagen.`;
+                        listItem.appendChild(para);
+                    }
+
+                    list.appendChild(listItem);
+                }
+            }
+        }
+        const fileTypes = [
+            "image/apng",
+            "image/bmp",
+            "image/gif",
+            "image/jpeg",
+            "image/jpg",
+            "image/pjpeg",
+            "image/png",
+            "image/svg+xml",
+            "image/tiff",
+            "image/webp",
+            "image/x-icon"
+        ];
+
+        function validFileType(file) {
+            return fileTypes.includes(file.type);
+        }
+
+        function returnFileSize(number) {
+            if (number < 1024) {
+                return `${number} bytes`;
+            } else if (number >= 1024 && number < 1048576) {
+                return `${(number / 1024).toFixed(1)} KB`;
+            } else if (number >= 1048576) {
+                return `${(number / 1048576).toFixed(1)} MB`;
+            }
+        }
+
+         // deshabilitar el boton de subida de ficheros:
+         eliminarImagen.onchange = function (){
+            const preview = document.querySelector('.preview');
+            input.disabled = this.checked;
+            if( this.checked ){
+                preview.classList.add("hidden")
+            }else{
+                preview.classList.remove("hidden")
+            }
 
         }
     </script>
