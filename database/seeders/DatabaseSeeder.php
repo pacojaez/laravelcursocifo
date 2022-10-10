37c5be50e1c539f33dad9c015c295f4a55dd99f2
Paco Jaez <58423341+pacojaez@users.noreply.github.com>
2022-10-10 19:53:34 +0200

    Done Bike and User seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,17 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\Bike::factory(200)->create();
         \App\Models\User::factory(20)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'ciudad' => 'Terrassa',
+            'provincia' => 'Barcelona',
+            'nacimiento' => '2000-03-25',
+            'telefono' => '606566636'
+            ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
